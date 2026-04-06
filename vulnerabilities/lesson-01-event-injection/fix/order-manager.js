5a1203a73b0935b22919ee0683763e466545d14e
yousefAlhadlaq <s202274280@kfupm.edu.sa>
2026-04-06 16:41:28 +0300

    Lesson 1: Event Injection (RCE) - exploit, fix, and verification ,Version2

--- vulnerabilities/lesson-01-event-injection/fix/order-manager.js
+++ vulnerabilities/lesson-01-event-injection/fix/order-manager.js
@@ -7,8 +7,23 @@ const jose = require('node-jose');
 exports.handler = (event, context, callback) => {
     // console.log(JSON.stringify(event));
     var req = JSON.parse(event.body);
-    var headers = JSON.parse(event.headers);
+    var headers = event.headers;
     var auth_header = headers.Authorization || headers.authorization;
+    var action = req.action;
+            const allowedActions = [
+                'new', 'update', 'cancel', 'get', 'orders',
+                'account', 'profile', 'shipping', 'billing',
+                'complete', 'inbox', 'message', 'delete',
+                'upload', 'feedback', 'admin-orders'
+            ];
+        
+            if (!allowedActions.includes(action)) {
+                return callback(null, {
+                    statusCode: 400,
+                    headers: { "Access-Control-Allow-Origin": "*" },
+                    body: JSON.stringify({ status: "err", message: "Invalid action" })
+                });
+            }
     var token_sections = auth_header.split('.');
     var auth_data = jose.util.base64url.decode(token_sections[1]);
     var token = JSON.parse(auth_data);
@@ -33,21 +48,6 @@ exports.handler = (event, context, callback) => {
                     isAdmin = userData.UserAttributes[i].Value;
                     break;
                 }
-            }
-            var action = req.action;
-            const allowedActions = [
-                'new', 'update', 'cancel', 'get', 'orders',
-                'account', 'profile', 'shipping', 'billing',
-                'complete', 'inbox', 'message', 'delete',
-                'upload', 'feedback', 'admin-orders'
-            ];
-        
-            if (!allowedActions.includes(action)) {
-                return callback(null, {
-                    statusCode: 400,
-                    headers: { "Access-Control-Allow-Origin": "*" },
-                    body: JSON.stringify({ status: "err", message: "Invalid action" })
-                });
             }
             var isOk = true;
             var payload = {};
